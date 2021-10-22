2bfe22508a586f33e14fff343ee4a0de4e4d09bf
AQB <abrown0926>
2021-10-21 21:48:42 -0500

    third commit

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,14 +3,14 @@ USE employees_db;
 INSERT INTO department SET name='Sales';
 INSERT INTO department SET name='Engineering';
 
-INSERT INTO role SET title= 'Marketing Manager',salary=120, department_id=1;
-INSERT INTO role SET title= 'Sales Agent',salary=40, department_id=1;
+INSERT INTO role SET title= 'Marketing Manager',salary=120000, department_id=1;
+INSERT INTO role SET title= 'Sales Agent',salary=40000, department_id=1;
 
-INSERT INTO role SET title= 'Senior Project Manager',salary=180, department_id=2;
-INSERT INTO role SET title= 'Project Manager',salary=120, department_id=2;
-INSERT INTO role SET title= 'Systems Engineer',salary=60, department_id=2;
-INSERT INTO role SET title= 'Software Engineer',salary=60, department_id=2;
-INSERT INTO role SET title= 'Intern',salary=40, department_id=2;
+INSERT INTO role SET title= 'Senior Project Manager',salary=180000, department_id=2;
+INSERT INTO role SET title= 'Project Manager',salary=120000, department_id=2;
+INSERT INTO role SET title= 'Systems Engineer',salary=60000, department_id=2;
+INSERT INTO role SET title= 'Software Engineer',salary=60000, department_id=2;
+INSERT INTO role SET title= 'Intern',salary=40000, department_id=2;
 
 INSERT INTO employee SET first_name= 'Tony' , last_name= 'B.', role_id=1;
 INSERT INTO employee SET first_name= 'Lebron' , last_name= 'J.', role_id=2, manager_id=1;
